3d52ee8df90092bd33a904070def27b99ef3a321
Harry Wright <haroldtomwright@gmail.com>
2017-12-06 15:49:32 +0000

    Initial Commit

---
+++ Predictable/Predictable.h
@@ -0,0 +1,19 @@
+//
+//  Predictable.h
+//  Predictable
+//
+//  Created by Harry Wright on 06/12/2017.
+//  Copyright © 2017 Off-Piste. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Predictable.
+FOUNDATION_EXPORT double PredictableVersionNumber;
+
+//! Project version string for Predictable.
+FOUNDATION_EXPORT const unsigned char PredictableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Predictable/PublicHeader.h>
+
+
