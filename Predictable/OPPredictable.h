5acb6427c536f1bb5e2adf96f38300c3ce06eb30
Harry Wright <haroldtomwright@gmail.com>
2017-12-06 15:59:01 +0000

    Base

---
+++ Predictable/OPPredictable.h
@@ -0,0 +1,13 @@
+//
+//  OPPredictable.h
+//  Predictable
+//
+//  Created by Harry Wright on 06/12/2017.
+//  Copyright © 2017 Off-Piste. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol OPPredictable <NSObject>
+
+@end
--- Predictable/Predictable.h
+++ Predictable/Predictable.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char PredictableVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Predictable/PublicHeader.h>
 
-
+#import <Predictable/OPPredictable>
